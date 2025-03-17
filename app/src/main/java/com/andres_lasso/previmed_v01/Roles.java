e46f0cc8e05581738b4360ad774bd19088e7a23d
JEISON101 <reyesjeison2006@gmail.com>
2025-03-17 18:06:02 -0500

    "NUEVO COLOR Y ROLES CASI TERMINADO"

--- app/src/main/java/com/andres_lasso/previmed_v01/Roles.java
+++ app/src/main/java/com/andres_lasso/previmed_v01/Roles.java
@@ -9,9 +9,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Roles extends AppCompatActivity {
+    private int id_rol;
+    public String nombre;
+    private String created;
+    private String updated;
 
-    public Roles() {
+    public Roles() {}
+    public Roles(int id_rol, String nombre, String created, String updated) {
+        this.id_rol = id_rol;
+        this.nombre = nombre;
+        this.created = created;
+        this.updated = updated;
     }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
