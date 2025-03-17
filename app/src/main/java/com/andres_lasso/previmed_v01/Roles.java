22177a4c8703adbaa9966cf6a39ce391a4cb5b34
JEISON101 <reyesjeison2006@gmail.com>
2025-03-17 16:57:40 -0500

    "Layout agregado"

---
+++ app/src/main/java/com/andres_lasso/previmed_v01/Roles.java
@@ -0,0 +1,22 @@
+package com.andres_lasso.previmed_v01;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Roles extends AppCompatActivity {
+
+    public Roles() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roles);
+
+    }
+}
