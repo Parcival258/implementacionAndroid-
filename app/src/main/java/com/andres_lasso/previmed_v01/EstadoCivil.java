7a90cfa69a84f87a121c8dd73462d45f72a2e07a
IngAlim2023 <enrique.porras@udea.edu.co>
2025-03-17 20:49:53 -0500

    Estado civil organizado

--- app/src/main/java/com/andres_lasso/previmed_v01/EstadoCivil.java
+++ app/src/main/java/com/andres_lasso/previmed_v01/EstadoCivil.java
@@ -1,6 +1,10 @@
 package com.andres_lasso.previmed_v01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EstadoCivil extends AppCompatActivity {
 
-    public int idestado_civil;
-    public String nombre_estado;
-
-    public EstadoCivil() {
-    }
-
-    public EstadoCivil(String nombre) {
-        this.nombre_estado = nombre;
-    }
-
-    public String getNombre_estado() {
-        return nombre_estado;
-    }
-
-    public void setNombre_estado(String nombre_estado) {
-        this.nombre_estado = nombre_estado;
-    }
-
-    public int getIdestado_civil() {
-        return idestado_civil;
-    }
+    EditText textEstadoCivil;
+    Button btnGuardarEstadoCivil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_civil);
+        btnGuardarEstadoCivil = findViewById(R.id.btnGuardarEstadoCivil);
+        textEstadoCivil = findViewById(R.id.textEstadoCivil);
 
+        btnGuardarEstadoCivil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Toast.makeText(EstadoCivil.this, "Estado civil creado", Toast.LENGTH_SHORT).show();
+                } catch (Exception e){
+                    Toast.makeText(EstadoCivil.this, "No se pudo crear el estado civil", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
