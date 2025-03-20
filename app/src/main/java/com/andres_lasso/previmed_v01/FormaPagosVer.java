12d9a84482fd89ee0b877741ec9706215e30f3d4
Miguelm27 <miguem27g@gmail.com>
2025-03-20 10:51:28 -0500

    cambioo

--- app/src/main/java/com/andres_lasso/previmed_v01/FormaPagosVer.java
+++ app/src/main/java/com/andres_lasso/previmed_v01/FormaPagosVer.java
@@ -4,14 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.core.view.WindowInsetsCompat;
-
-public class Forma_pagosVer extends AppCompatActivity {
+public class FormaPagosVer extends AppCompatActivity {
 
     Button btnpago;
 
@@ -23,7 +19,7 @@ public class Forma_pagosVer extends AppCompatActivity {
         btnpago.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Forma_pagosVer.this, Forma_pagos.class);
+                Intent intent = new Intent(FormaPagosVer.this, FormaPagos.class);
                 startActivity(intent);
             }
         });
