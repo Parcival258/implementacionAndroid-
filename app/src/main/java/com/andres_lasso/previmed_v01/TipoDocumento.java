f9118cd22bbf56c365d37a66f31d0a34ba66e8f0
parvical258 <andreslasso250@gmail.com>
2025-03-20 10:49:18 -0500

    Tipos documentos.v2

--- app/src/main/java/com/andres_lasso/previmed_v01/TipoDocumento.java
+++ app/src/main/java/com/andres_lasso/previmed_v01/TipoDocumento.java
@@ -1,5 +1,6 @@
 package com.andres_lasso.previmed_v01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,32 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TipoDocumento{
-    public int id;
-    public String tipo;
+public class TipoDocumento extends AppCompatActivity {
 
-    public  TipoDocumento(){
-
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tipos_documento);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
-
-
-    public TipoDocumento(String tipo, int id){
-        this.tipo = tipo;
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-
-
 }
